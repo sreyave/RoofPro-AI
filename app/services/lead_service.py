4bcc2cca691833f7534d2c61e38b7595ba529fda
Sreya Dhar <sreyadhar@virtualemployee.com>
2026-04-16 12:50:34 +0530

    Updated admin login UI with demo info box and improved styling

--- app/services/lead_service.py
+++ app/services/lead_service.py
@@ -1,21 +1,38 @@
+import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
 
-def get_all_leads():
+def get_client():
     scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
     ]
 
+    # ----------------------------
+    # LOCAL ENVIRONMENT
+    # ----------------------------
+    if os.path.exists("credentials.json"):
+        creds = Credentials.from_service_account_file(
+            "credentials.json",
+            scopes=scope
+        )
 
-    creds = Credentials.from_service_account_file(
-        "credentials.json", scopes=scope
-    )
+    # ----------------------------
+    # RENDER / PRODUCTION
+    # ----------------------------
+    else:
+        creds_json = json.loads(os.environ["GOOGLE_CREDENTIALS_JSON"])
 
-    client = gspread.authorize(creds)
+        creds = Credentials.from_service_account_info(
+            creds_json,
+            scopes=scope
+        )
 
+    return gspread.authorize(creds)
+
+
+def get_all_leads():
+    client = get_client()
     sheet = client.open("Roofing Leads").sheet1
-
-    records = sheet.get_all_records()
-
-    return records
+    return sheet.get_all_records()
